12d7c11791ed79d85726085cc056783f63dc9dff
andrescvargasr <andresc.vargasr@gmail.com>
2023-01-19 12:43:01 -0500

    feat: ex. 3.5

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ app.use(express.json());
 
 let persons = [
   { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
+    id: 1,
+    name: "Arto Hellas", 
+    number: "040-123456"
   },
   { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+    id: 2,
+    name: "Ada Lovelace", 
+    number: "39-44-5323523"
   },
   { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
+    id: 3,
+    name: "Dan Abramov", 
+    number: "12-43-234345"
   },
   { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
+    id: 4,
+    name: "Mary Poppendieck", 
+    number: "39-23-6423122"
   }
 ]
 
@@ -65,35 +65,37 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
+  const idRamdom = Math.floor(Math.random() * 100000);
+  const maxId = persons.length > 0
+    ? persons.find(person => person.id === idRamdom)
+      ? generateId()
+      : idRamdom
+    : 1;
+  
+  return maxId;
 }
 
-
 /* A post request that is sending the data to the server. */
-app.post('/api/notes', (request, response) => {
-  
+app.post('/api/persons', (request, response) => {
   
   const body = request.body;
+  // console.log(body);
 
-  if (!body.content) {
+  if (!body.name) {
     return response.status(400).json({
       error: 'content missing'
     })
   }
 
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
+  const person = {
+    name: body.name,
+    number: body.number,
     id: generateId(),
   }
 
-  notes = notes.concat(note);
-  console.log(note);
-  response.json(note);
+  persons = persons.concat(person);
+  console.log(person);
+  response.json(person);
 })
 
 const PORT = 3001;
